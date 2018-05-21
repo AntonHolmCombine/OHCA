60bf3c8d65cd9d48d145424a7ab7ea8bc0e94131
unknown <Anton.Holm@combine.se>
2018-05-21 13:15:52 +0200

    Scripts for plotting Trees with Graphviz

---
+++ Cforest_tree.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from Tree import Node, Leaf, Edge, set_i
+
+
+def export_dot(tree, filename):
+    set_i()
+    from graphviz import Graph
+
+    u = Graph('G', filename=filename + "_dot", format='png')
+    u.edge_attr.update(arrowhead='vee', arrowsize='2')
+
+    u.node_attr.update(color='lightblue2', style='filled')
+    tree.print_dot(u, tree.nbr_samples)
+    u.view()
+
+
+def populate_tree(input_data, tree):
+    for j in range(0, len(input_data)):
+        row = input_data.iloc[j]
+        tree.add_data(row)
+
+
+def make_tree(filename):
+    fid = open(filename + ".txt", 'r')
+    node = Node('0', None)
+    node.add_left(Edge(None, None))
+    t = {}
+    stack = [node]
+    return build_tree(fid, stack, t)
+
+
+def build_tree(fid, stack, tree):
+    if stack == [] and tree:
+        return tree
+    last = stack[-1]
+    if not last.left.is_connected() or (last.right is None or not
+                                        last.right.is_connected()):
+
+        line = fid.readline().replace('"', '').replace('\n', '').replace(' \\n', '')
+        if len(line) < 2:
+            return tree
+        if ';' in line:
+            [first, second] = line.lstrip(' ').split(';')
+            [id_, name_, comp, cond] = first.split(' ')
+            [criterion, statistic] = second.split(',')
+            criterion = criterion.replace(' criterion = ', '')
+            statistic = statistic.replace(' statistic = ', '')
+            id_ = id_.rstrip(')')
+            node = Node(id_, name_, criterion, statistic)
+
+            if not last.left.is_connected():
+                last.left.add_node(node)
+            else:
+                last.right.add_node(node)
+
+            if id_ in tree:
+                print("Something wrong")
+            else:
+                tree[id_] = node
+
+            edge = Edge(cond, comp == '<=')
+            node.add_left(edge)
+
+            stack.append(node)
+
+            return build_tree(fid, stack, tree)
+
+        # Right branch
+        elif '*' not in line:
+            [id_, name_, comp, cond] = line.lstrip(' ').replace('\n',
+                                                                '').split(' ')
+            id_ = id_.rstrip(')')
+            if id_ not in tree:
+                tree[id_] = Node(id_, name_)
+            node = tree[id_]
+            edge = Edge(cond, comp == '<=')
+            node.add_right(edge)
+            return build_tree(fid, stack, tree)
+
+        # leaf
+        elif '*' in line:
+            [id_, second] = line.lstrip(' ').split('*')
+            a = second.replace('\n', '').rstrip(' ').split(' ')
+            id_ = id_.rstrip(')')
+            weight = a[len(a) - 1]
+            leaf = Leaf(id_, weight)
+            tree[id_] = leaf
+            if not last.left.is_connected():
+                edge = last.left
+            else:
+                edge = last.right
+            edge.add_leaf(leaf)
+            return build_tree(fid, stack, tree)
+    else:
+        stack.pop(-1)
+        return build_tree(fid, stack, tree)
+
+
+if __name__ == '__main__':
+    for i in range(1, 19):
+        name = 'trees/TreeTxt_{}'.format(i)
+        root = make_tree(name)
+        data = pd.read_excel("Imputed_Data.xlsx")
+        populate_tree(data, root['1'])
+        export_dot(root['1'], name)
---
+++ Tree.py
@@ -0,0 +1,154 @@
+import numpy as np
+global i
+
+
+def set_i():
+    global i
+    i = 0
+
+
+class Node:
+    def __init__(self, id, name, criterion=None,
+                 statistic=None):
+        self.id = id
+        self.name = name
+        self.criterion = criterion
+        self.statistic = statistic
+
+        self.left = None
+        self.right = None
+        self.nbr_samples = 0
+
+    def add_data(self, row):
+        self.nbr_samples += 1
+        self.left.add_data(row, self.name)
+        self.right.add_data(row, self.name)
+
+    def add_left(self, node):
+        self.left = node
+
+    def add_right(self, node):
+        self.right = node
+
+    def print(self):
+
+        print(self.id, self.name, 'nsamples =',
+              self.nbr_samples)
+        if self.left:
+            self.left.print()
+        if self.right:
+            self.right.print()
+
+    def print_dot(self, u, samples):
+        global i
+
+        traffic = round(self.nbr_samples/samples, 2)
+        u.node(str(i), label=self.name + "\n" + "Samples = " + str(
+            self.nbr_samples), color=(".55 " + str(traffic) + " 0.999"))
+        a = str(i)
+
+        if self.left:
+            i += 1
+            self.left.print_dot(u, a, samples)
+
+        if self.right:
+            i += 1
+            self.right.print_dot(u, a, samples)
+
+
+class Edge:
+    def __init__(self, condition, less_than):
+        self.condition = condition
+        self.less_than = less_than
+        self.node = None
+        self.leaf = None
+        self.connected = False
+
+    def add_data(self, row, name):
+        curr = row[name]
+
+        if (curr <= float(self.condition) and self.less_than) or (curr > float(
+                self.condition) and not self.less_than):
+            if self.node:
+                self.node.add_data(row)
+            else:
+                self.leaf.add_data(row)
+
+    def is_connected(self):
+        return self.connected
+
+    def add_node(self, node):
+        self.node = node
+        self.connected = True
+
+    def add_leaf(self, leaf):
+        self.leaf = leaf
+        self.connected = True
+
+    def print(self):
+        if self.less_than:
+            comp = '<='
+        else:
+            comp = '>'
+        print(comp, self.condition)
+        if self.node:
+            self.node.print()
+        elif self.leaf:
+            self.leaf.print()
+        else:
+            print("Something wrong")
+
+    def print_dot(self, u, a, samples):
+        if self.less_than:
+            comp = '<='
+        else:
+            comp = '>'
+
+        if self.node:
+            traffic = self.node.nbr_samples
+        else:
+            traffic = self.leaf.nbr_samples
+
+        #penwidth = traffic/samples*10
+        u.edge(a, str(i), label=" " + comp + " " + str(
+            self.condition) + " ", splines='polyline')
+
+        if self.node:
+            self.node.print_dot(u, samples)
+        elif self.leaf:
+            self.leaf.print_dot(u, samples)
+        else:
+            print("Something wrong")
+
+
+class Leaf:
+    def __init__(self, id, weight):
+        self.id = id
+        self.weight = weight
+        self.nbr_samples = 0
+        self.good = 0
+        self.bad = 0
+
+    def add_data(self, row):
+        self.nbr_samples += 1
+        if row['Binary_Sec_Out_180_day_CPC_score'] == 0:
+            self.good += 1
+        else:
+            self.bad += 1
+
+    def print(self):
+        print(self.id, self.weight, 'nsamples = ', self.nbr_samples)
+
+    def print_dot(self, u, samples):
+        traffic = round(self.nbr_samples / samples, 2)
+
+        if self.good > self.bad:
+            u.node(str(i), label="Samples = " + str(self.nbr_samples) + "\n" +
+                   str(np.round(self.good/self.nbr_samples, 2)*100) + "% Good",
+                   shape='diamond', fillcolor=(".55 " + str(traffic) +
+                                               " 0.999"), color='green')
+        else:
+            u.node(str(i), label="Samples = " + str(self.nbr_samples) + "\n" +
+                   str(np.round(self.bad / self.nbr_samples, 2)*100) +
+                   "% Poor", shape='box', color='red', fillcolor=(
+                ".55 " + str(traffic) + " 0.999"), )
