89e6f9c5c031e4676b5ad303d219e8aa9be573cb
unknown <Anton.Holm@combine.se>
2018-05-22 15:29:19 +0200

    Bug Fix

--- parseTree.R
+++ parseTree.R
@@ -1,13 +1,13 @@
 parseTree <- function(x.model){
   
-  for (i in 1:length(x.model@ensemble)){
+  for (i in 37:length(x.model@ensemble)){
     
     atree <- prettytree(x.model@ensemble[[i]], names(x.model@data@get("input"))) 
     
     con <- file(paste("TreeTxt_",i,".txt",sep=""))
     sink(con,append=TRUE)
     print(atree)
-    
+    sink()
   }
   sink()
   
