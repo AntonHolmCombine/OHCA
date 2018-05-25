c7eb433853757088c0fd6c6c6be28ffd904c18c0
unknown <Anton.Holm@combine.se>
2018-05-25 15:20:52 +0200

    Color changes and QOL changes

--- pieChartPlot.py
+++ pieChartPlot.py
@@ -9,29 +9,41 @@ for k in range(0,len(files)):
 	data = pd.read_csv(filename,sep=" ",header = None, skiprows=1)
 
 	nrows = data.shape[0]
-
-	
+	tp = open(filename,"r")
+	vName = next(tp)
 	concentric = True
 
 	if(concentric):
-		if(nrows==2):
+		if(vName=="B Sex\n"):
 			fig, ax = plt.subplots(figsize=(9.0,6.0))
 			plt.rcParams["font.size"]=13.0
 			for i in range(0,nrows):
 				sizes = data.iloc[i,1:3]
-				ax.pie(sizes,radius=1-(0.4*(nrows-(i+1))),colors=["green","red"],wedgeprops=dict(width=0.4,edgecolor="w"),pctdistance=0.67+0.05*(i+1))
+				ax.pie(sizes,radius=1-(0.4*(nrows-(i+1))),colors=["green","red"],wedgeprops=dict(width=0.4,edgecolor="w"))
+				ax.text(1-(0.4*(nrows-(i+1)))-0.4,0.05+(0.09*i),str(data.iloc[i,0]),fontsize=13)
+
+			ax.set(aspect="equal")
+			plt.legend(["Good","Bad"],loc="best")
+		elif(nrows==2):
+			fig, ax = plt.subplots(figsize=(9.0,6.0))
+			plt.rcParams["font.size"]=13.0
+			for i in range(0,nrows):
+				sizes = data.iloc[i,1:3]
+				ax.pie(sizes,radius=1-(0.4*(nrows-(i+1))),colors=[(0,0.85,0,0.4),(1,0,0,0.5)],wedgeprops=dict(width=0.4,edgecolor="w"))
 				ax.text(1-(0.4*(nrows-(i+1)))-0.4,0.05,str(data.iloc[i,0]),fontsize=13)
 
 			ax.set(aspect="equal")
+			plt.legend(["Good","Bad"],loc="best")
 		else:
 			fig, ax = plt.subplots(figsize=(12.0,9.0))
 			plt.rcParams["font.size"]=13.0
 			for i in range(0,nrows):
 				sizes = data.iloc[i,1:3]
-				ax.pie(sizes,radius=1-(0.2*(nrows-(i+1))),colors=["green","red"],wedgeprops=dict(width=0.2,edgecolor="w"),pctdistance=0.67+0.05*(i+1))
+				ax.pie(sizes,radius=1-(0.2*(nrows-(i+1))),colors=["green","red"],wedgeprops=dict(width=0.2,edgecolor="w"))
 				ax.text(1-(0.2*(nrows-(i+1)))-0.2-(0.01*i),0.05+(0.09*i),str(data.iloc[i,0]),fontsize=13)
 
 			ax.set(aspect="equal")
+			plt.legend(["Good","Bad"],loc="best")
 
 	else:
 		plt.rcParams["font.size"]=13.0
@@ -51,10 +63,10 @@ for k in range(0,len(files)):
 
 			plt.axis("equal")
 		plt.subplots_adjust(left=0.11,right=0.90,bottom=0.05,top=0.85,wspace=0.20)
+		plt.legend(["Good","Bad"],loc="lower right",bbox_to_anchor=(1.2,-0.05))
+	
 
-	tp = open(filename,"r")
+	plt.suptitle(vName,fontsize=25)
 
-	plt.suptitle(next(tp),fontsize=25)
-	plt.legend(["Good","Bad"],loc="lower right",bbox_to_anchor=(1.2,-0.05))
 	#plt.show()
 	plt.savefig(filename[:-4]+".png",format="png",dpi=200)
