e8c92db9afff287d985a838c3577086575f9bf42
unknown <Anton.Holm@combine.se>
2018-05-29 10:29:52 +0200

    Better patient handling

--- Movie/TreeToSteps.py
+++ Movie/TreeToSteps.py
@@ -2,22 +2,24 @@ import glob
 import os
 from shutil import copyfile
 
-trees = glob.glob(os.getcwd()+"\\tree_*")
+patient = "pat_0"
+workdir = os.getcwd()+"\\"+patient+"\\"
+trees = glob.glob(workdir+"tree_*")
 nbrOfTrees = len(trees)
 allTerminal = False
 terminals = []
 stepTracker = 0
 
 while(not(allTerminal)):
-	os.mkdir(os.getcwd() + "\\step_" + str(stepTracker))
+	os.mkdir(workdir + "step_" + str(stepTracker))
 	for currTree in trees:
 		if(currTree in terminals):
 			currLen = len(glob.glob(currTree+"\\*.png"))
 			copyfile(currTree+"\\patient_depth_"+str(currLen-1)+".png",
-				os.getcwd()+"\\step_"+str(stepTracker)+"\\tree_"+str(currTree.split("tree_")[1])+"_step_"+str(stepTracker)+".png")
+				workdir+"step_"+str(stepTracker)+"\\tree_"+str(currTree.split("tree_")[1])+"_step_"+str(stepTracker)+".png")
 		else:
 			copyfile(currTree+"\\patient_depth_"+str(stepTracker)+".png",
-				os.getcwd()+"\\step_"+str(stepTracker)+"\\tree_"+str(currTree.split("tree_")[1])+"_step_"+str(stepTracker)+".png")
+				workdir+"step_"+str(stepTracker)+"\\tree_"+str(currTree.split("tree_")[1])+"_step_"+str(stepTracker)+".png")
 			currLen = len(glob.glob(currTree+"\\*.png"))
 			if(currLen-1<=stepTracker):
 				terminals.append(currTree)
